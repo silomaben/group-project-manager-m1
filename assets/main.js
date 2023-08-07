e559d9d2ad979c25cf2eca236afb8ff7c243e9d7
mjwachira <mjwachira1@gmail.com>
2023-08-07 18:00:28 +0300

    hamburgar menu

--- assets/main.js
+++ assets/main.js
@@ -15,3 +15,20 @@ window.addEventListener('click', (event) => {
     modal.style.display = 'none';
   }
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
